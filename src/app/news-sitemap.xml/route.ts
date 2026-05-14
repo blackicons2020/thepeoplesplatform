db7190febe3a2918850ffce9aefd2b003d25845d
Emeke John <jomekgroup@gmail.com>
2026-05-14 13:59:00 +0100

    seo: refine news sitemap XML for better bot compatibility

--- src/app/news-sitemap.xml/route.ts
+++ src/app/news-sitemap.xml/route.ts
@@ -18,21 +18,32 @@ export async function GET() {
   const SITE_NAME = "The People's Platform";
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
-  ${articles.map((article: any) => `
-  <url>
-    <loc>${SITE_URL}/${(article.slug || '').replace(/-\d{13}$/, '')}</loc>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
+${articles.map((article: any) => {
+  const title = article.title
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;');
+    
+  // Format date to W3C format without milliseconds (YYYY-MM-DDThh:mm:ssZ)
+  const date = article.date.toISOString().split('.')[0] + 'Z';
+  const loc = `${SITE_URL}/${(article.slug || '').replace(/-\d{13}$/, '')}`;
+
+  return `  <url>
+    <loc>${loc}</loc>
     <news:news>
       <news:publication>
         <news:name>${SITE_NAME}</news:name>
         <news:language>en</news:language>
       </news:publication>
-      <news:publication_date>${article.date.toISOString()}</news:publication_date>
-      <news:title>${article.title.replace(/&/g, '&amp;')}</news:title>
+      <news:publication_date>${date}</news:publication_date>
+      <news:title>${title}</news:title>
     </news:news>
-  </url>`).join('')}
-</urlset>`;
+  </url>`;
+}).join('\n')}
+</urlset>`.trim();
 
   return new NextResponse(xml, {
     headers: {
